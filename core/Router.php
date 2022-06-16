7054bce8897d9331f0f5f1312ee6be8ea6ca58b9
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-16 18:43:24 +0430

    add get and post to router class

--- core/Router.php
+++ core/Router.php
@@ -2,6 +2,13 @@
 namespace core;
 
 class Router{
+    public array $routers;
+    public function get($path,$callback){
+        $this->routers['get'][$path]=$callback;
+    }
+    public function post($path,$callback){
+        $this->routers['post'][$path]=$callback;
+    }
     public function resolve(){
         return "Router";
     }
