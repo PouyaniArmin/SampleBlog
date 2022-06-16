04f60ae19fc058ea449fcf3404c74372ab02ffac
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-16 18:18:41 +0430

    add object router

--- core/Application.php
+++ core/Application.php
@@ -1,12 +1,12 @@
 <?php 
 namespace core;
-
+use core\Router;
 class Application{
     public Router $router;
     public function __construct(){
-        $this->router=new Router;
+    $this->router=new Router();
     }
     public function run(){
-        echo $this->resolve();
+        echo $this->router->resolve();
     }
 }
