d831b037ed273a5ca818eb770f48d867b13c975d
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-16 18:39:19 +0430

    add url mehod body to Request class

--- core/Request.php
+++ core/Request.php
@@ -8,12 +8,19 @@ class Request{
         if(!$position){
             return $path;
         }
-        echo substr($path,0,$position);
+        return substr($path,0,$position);
     }
     public function method(){
-        echo strtolower($_SERVER['REQUEST_METHOD']);
+        return strtolower($_SERVER['REQUEST_METHOD']);
     }
     public function body(){
-
+        $body=[];
+        foreach($_GET as $key=>$value){
+            $body[$key]=$value;
+        }
+        foreach ($_POST as $key=>$value){
+            $body[$key]=$value;
+        }
+        return $body;
     }
 }
