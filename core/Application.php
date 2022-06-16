415c517e3ac458cd500e28f5cf2e97999076fc1a
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-16 18:35:07 +0430

    applcation

--- core/Application.php
+++ core/Application.php
@@ -1,10 +1,13 @@
 <?php 
 namespace core;
 use core\Router;
+use core\Request;
 class Application{
     public Router $router;
+    public Request $request;
     public function __construct(){
     $this->router=new Router();
+    $this->request=new Request();
     }
     public function run(){
         echo $this->router->resolve();
--- core/Request.php
+++ core/Request.php
@@ -3,10 +3,15 @@ namespace core;
 
 class Request{
     public function path(){
-        
+        $path=$_SERVER['REQUEST_URI'] ?? '';
+        $position=strpos($path,'?');
+        if(!$position){
+            return $path;
+        }
+        echo substr($path,0,$position);
     }
     public function method(){
-
+        echo strtolower($_SERVER['REQUEST_METHOD']);
     }
     public function body(){
 
