f7e229d879dd1f3157120f8df89748d3e3114b81
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-18 10:20:23 +0430

    pass dir to applcation class

--- core/Application.php
+++ core/Application.php
@@ -5,7 +5,9 @@ use core\Request;
 class Application{
     public Router $router;
     public Request $request;
-    public function __construct(){
+    public static Application $app;
+    public function __construct($path){
+        self::$app=$this;
     $this->request=new Request();
     $this->router=new Router($this->request);
     }
