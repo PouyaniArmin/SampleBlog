32442b6482e49eca556e60f34c2af155237e9e3f
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-18 12:31:53 +0430

    object response to application

--- core/Application.php
+++ core/Application.php
@@ -2,8 +2,10 @@
 namespace core;
 use core\Router;
 use core\Request;
+use core\Response;
 class Application{
     public Router $router;
+    public Response $response;
     public Request $request;
     public static string $path;
     public static Application $app;
@@ -11,7 +13,8 @@ class Application{
     self::$app=$this;
     self::$path=$path;
     $this->request=new Request();
-    $this->router=new Router($this->request);
+    $this->response=new Response();
+    $this->router=new Router($this->request,$this->response);
     }
     public function run(){
         echo $this->router->resolve();
