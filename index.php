7991fe710fc9570bd4fa9b108ffa0638dbcaf9b7
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-19 18:44:16 +0430

    index add doc

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ use core\Application;
 require_once './autoLoader/AutoLoader.php';
 $app=new Application(__DIR__);
 $app->router->get('/',[HomeController::class,'index']);
-$app->router->get('/documents',[DocumentController::class,'index']);
+$app->router->get('/document',[DocumentController::class,'index']);
 $app->run();
