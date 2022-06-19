61374ffdf223f23c1077df4ad6288c403fea7c4c
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-19 17:55:55 +0430

    add setter and getter controller

--- core/Application.php
+++ core/Application.php
@@ -3,10 +3,12 @@ namespace core;
 use core\Router;
 use core\Request;
 use core\Response;
+use core\Controller;
 class Application{
     public Router $router;
     public Response $response;
     public Request $request;
+    public Controller $controller;
     public static string $path;
     public static Application $app;
     public function __construct($path){
@@ -16,6 +18,14 @@ class Application{
     $this->response=new Response();
     $this->router=new Router($this->request,$this->response);
     }
+
+    public function getController():Controller{
+        return $this->controller;
+    }
+    public function setController(Controller $controller){
+        $this->controller=$controller;
+    }
+
     public function run(){
         echo $this->router->resolve();
     }
