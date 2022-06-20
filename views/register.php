0b9d861fd8b8f6b3a8daad3721a4212b0a078a8c
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-20 11:12:36 +0430

    rigster form

--- views/register.php
+++ views/register.php
@@ -1,27 +1,35 @@
-<form>
-<div class="row">
-    <div class="col">
-    <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-  </div>    
-    </div>
-    <div class="col">
-    <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-  </div>
-    </div>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-check">
-  </div>
-  <br>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<div class="container">
+    <form>
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <label>FirstName</label>
+                    <input type="text" class="form-control" placeholder="Enter firstname" name="firstnmae">
+                </div>
+            </div>
+            <div class="col">
+                <div class="form-group">
+                    <label>LastName</label>
+                    <input type="text" class="form-control"placeholder="Enter LastName" name="lastname">
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="form-group">
+            <label>Email address</label>
+            <input type="email" class="form-control" placeholder="Enter email" name="email">
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <br>
+        <div class="form-group">
+            <label>Confirm Password</label>
+            <input type="password" class="form-control" placeholder="Confirm Password" name="confirmPassword">
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
