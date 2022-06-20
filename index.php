1825936414a8e35f8eaea6ea5e70b2700804399f
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-20 10:57:36 +0430

    index

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ use controllers\DocumentController;
 use controllers\HomeController;
 use core\Application;
 use controllers\LoginController;
+use controllers\RegisterController;
 
 require_once './autoLoader/AutoLoader.php';
 $app=new Application(__DIR__);
 $app->router->get('/',[HomeController::class,'index']);
 $app->router->get('/document',[DocumentController::class,'index']);
 $app->router->get('/login',[LoginController::class,'index']);
-$app->router->get('/register',[LoginController::class,'index']);
+$app->router->get('/register',[RegisterController::class,'index']);
 $app->run();
