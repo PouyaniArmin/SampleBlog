a00fb6f517f20776909dff5118cde6eeafad20f0
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-20 11:01:40 +0430

    form

--- views/register.php
+++ views/register.php
@@ -1,10 +1,16 @@
 <form>
 <div class="row">
     <div class="col">
-      1 of 2
+    <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>    
     </div>
     <div class="col">
-      2 of 2
+    <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
     </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
@@ -15,8 +21,7 @@
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
   <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
+  <br>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
