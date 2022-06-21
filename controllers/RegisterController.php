0d46e1f2a6e4b7231ad120400832999b4a7d31e6
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-21 09:56:28 +0430

    model

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -2,9 +2,15 @@
 namespace controllers;
 
 use core\Controller;
+use core\Request;
 
 class RegisterController extends Controller{
-    public function index(){
+    public function index(Request $request){
+        if($request->isPost()){
+        $body=$request->body();
+        var_dump($body);
+        exit;
+    }
         return $this->view('register');
     }
 }
