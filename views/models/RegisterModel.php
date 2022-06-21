02f00641e857385b32392a41acdce40f2774171d
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-21 11:33:58 +0430

    rule

--- views/models/RegisterModel.php
+++ views/models/RegisterModel.php
@@ -1,8 +1,20 @@
 <?php 
 namespace models;
 
-class RegisterModel{
+use core\Model;
+
+class RegisterModel extends Model{
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+    public string $confirmPassword;
+
     public function register(){
         return "submitted";
+    }
+
+    public function rules():array{
+        return[];
     }
 }
