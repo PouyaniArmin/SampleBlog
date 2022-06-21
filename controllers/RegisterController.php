140a2d8fdcef51265f3a9b80e6144b824b080827
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-21 11:31:03 +0430

    object register model

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -3,9 +3,11 @@ namespace controllers;
 
 use core\Controller;
 use core\Request;
+use models\RegisterModel;
 
 class RegisterController extends Controller{
     public function index(Request $request){
+       $registerModel=new RegisterModel();
         if($request->is_Post()){
             $body=$request->body();
             var_dump($body);
