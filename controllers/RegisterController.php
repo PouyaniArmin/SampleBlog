2b5feb2b804d10c73d63ee30920596fc95ba507c
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-21 10:50:20 +0430

    get input form

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -6,7 +6,11 @@ use core\Request;
 
 class RegisterController extends Controller{
     public function index(Request $request){
-    
+        if($request->is_Post()){
+            $body=$request->body();
+            var_dump($body);
+            exit;
+        }
         return $this->view('register');
         
     }
