de477b21c2b8abf0dcd30e2ced3d8e07f5469dac
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-21 11:00:19 +0430

    model vaidate

--- core/Model.php
+++ core/Model.php
@@ -1,5 +1,22 @@
-<?php 
+<?php
+
 namespace core;
- class Model{
-    
- }
+
+abstract class Model
+{
+    public array $errors;
+    public function loadData($data)
+    {
+        foreach($data as $key=>$value){
+            if(property_exists($this,$key)){
+                $this->{$key}=$value;
+            }
+        }
+    }
+
+    abstract function rules():array;
+
+    public function validate(){
+        
+    }
+}
