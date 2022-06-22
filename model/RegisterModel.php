05dfdd4eab72a29fa1f6dd6878e3be4d9f1213f7
=PouyaniArmin <=pouyaniarmin@gmail.com>
2022-06-22 11:43:41 +0430

    create rules register

--- model/RegisterModel.php
+++ model/RegisterModel.php
@@ -4,10 +4,22 @@ namespace model;
 use core\Model;
 
 class RegisterModel extends Model{
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+    public string $confirmPassword;
+    
     public function register(){
         return "success";
     }
     public function rules():array{
-        return [];
+        return [
+            'firstname'=>[self::RULE_REQUIRED],
+            'lastname'=>[self::RULE_REQUIRED],
+            'email'=>[self::RULE_REQUIRED,self::RULE_EMAIL],
+            'password'=>[self::RULE_REQUIRED,[self::RULE_MIN,'min'=>6],[self::RULE_MAX,'max'=>24]],
+            'confirmPassword'=>[self::RULE_REQUIRED,[self::RULE_MATCH,'match'=>'password']]
+        ];
     }
 }
